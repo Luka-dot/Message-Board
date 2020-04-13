af1b4dd11fbbacf297b1975286e74ac7ffc5956c
Luka-dot <58044174+Luka-dot@users.noreply.github.com>
2020-04-13 19:21:31 -0400

    server side changes

--- MessageBoard-API/app.js
+++ MessageBoard-API/app.js
@@ -6,20 +6,21 @@ app.use(express.json());
 app.use(cors());
 
 app.locals.notes = [
+  
   { id: shortid.generate(),
-    title: 'Evaluation To-Do',
+    title: 'First To-Do',
     background: '#ffe680',
     list: [
       { id: shortid.generate(),
-        text: 'Weep with joy at our success',
-        isComplete: false
+        text: 'Create new To-Do',
+        isComplete: true
       },
       { id: shortid.generate(),
-        text: 'Applaud and bow',
-        isComplete: false
+        text: 'Test it',
+        isComplete: true
       },
       { id: shortid.generate(),
-        text: 'Give us perfect scores on everything',
+        text: 'Make changes to make it better',
         isComplete: false
       },
       { id: shortid.generate(),
@@ -29,217 +30,34 @@ app.locals.notes = [
     ]
   },
   { id: shortid.generate(),
-    title: 'Team To-Do',
-    background: '#ffe680',
+    title: 'Shopping list',
+    background: '#EEF5DB',
     list: [
-      { id: shortid.generate(),
-        text: 'Show project at demo night',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Get jobs',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Keep nailing it',
-        isComplete: true
-      },
-      { id: shortid.generate(),
-        text: '',
-        isComplete: false
-      },
-    ]
-  },
-  { id: shortid.generate(),
-    title: 'Shopping List',
-    background: '#eef5db',
-    list: [
-      { id: shortid.generate(),
-        text: 'Apples',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Carrots',
-        isComplete: true
-      },
       { id: shortid.generate(),
         text: 'Bread',
         isComplete: false
       },
       { id: shortid.generate(),
         text: 'Milk',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Beer',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Vanilla',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Oats',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Pom Seeds',
         isComplete: true
       },
       { id: shortid.generate(),
-        text: '',
-        isComplete: false
-      },
-    ]
-  },
-  { id: shortid.generate(),
-    title: 'Supplies for Birthday Party',
-    background: '#d6ffb7',
-    list: [
-      { id: shortid.generate(),
-        text: 'Balloons',
+        text: 'Cereals',
         isComplete: false
       },
       { id: shortid.generate(),
-        text: 'Cake',
-        isComplete: true
-      },
-      { id: shortid.generate(),
-        text: 'Party Hats',
+        text: 'Oranges',
         isComplete: false
       },
       { id: shortid.generate(),
-        text: 'Pinata',
-        isComplete: true
-      },
-      { id: shortid.generate(),
-        text: 'Streamers',
+        text: 'Ice-Cream',
+        isComplete: false
+      },{ id: shortid.generate(),
+        text: 'Popcorn',
         isComplete: false
       },
       { id: shortid.generate(),
-        text: 'Chairs',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Cups',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Tables',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Unicorn Candles',
-        isComplete: true
-      },
-      { id: shortid.generate(),
-        text: '',
-        isComplete: false
-      },
-    ]
-  },
-  { id: shortid.generate(),
-    title: 'How to Make Toast',
-    background: '#eef5db',
-    list: [
-      { id: shortid.generate(),
-        text: 'Get a job so you can buy a toaster',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Buy a toaster',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Buy bread',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Put bread in toaster for 2 mins',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Remove bread from toaster when golden brown',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Spread small pad of butter on toast',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Enjoy!',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: '',
-        isComplete: false
-      },
-    ]
-  },
-  { id: shortid.generate(),
-    title: 'Archie ToDo Daily',
-    background: '#d6ffb7',
-    list: [
-      { id: shortid.generate(),
-        text: 'Eat Breakfast',
-        isComplete: true
-      },
-      { id: shortid.generate(),
-        text: 'Play with Finn',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Poop',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Pee',
-        isComplete: true
-      },
-      { id: shortid.generate(),
-        text: 'Chew on bone',
-        isComplete: true
-      },
-      { id: shortid.generate(),
-        text: 'Eat Dinner',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: '',
-        isComplete: false
-      },
-    ]
-  },
-  { id: shortid.generate(),
-    title: 'Job Prep',
-    background: '#ffe680',
-    list: [
-      { id: shortid.generate(),
-        text: 'Start Resume',
-        isComplete: true
-      },
-      { id: shortid.generate(),
-        text: 'Send resume to Brenna for review',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Update LinkedIn',
-        isComplete: true
-      },
-      { id: shortid.generate(),
-        text: 'Go on Job Shadow',
-        isComplete: true
-      },
-      { id: shortid.generate(),
-        text: 'Warm Outreach',
-        isComplete: true
-      },
-      { id: shortid.generate(),
-        text: 'Beer date with Gabi',
-        isComplete: false
-      },
-      { id: shortid.generate(),
-        text: 'Apply for jobs',
+        text: 'Coffee',
         isComplete: false
       },
       { id: shortid.generate(),
